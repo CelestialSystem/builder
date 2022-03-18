4f26ed4e58fa22897a0aadba74a3c2bde2ea8a9e
sibythomas <siby.t@celestialsys.com>
2022-03-18 14:27:16 +0530

    Css styles chanes and bug fix

--- src/html/MainView.ts
+++ src/html/MainView.ts
@@ -9,7 +9,7 @@ export function getMainViewHtml(){
             </div>
             <div id="config-section">
                 <div class="filter-section">
-                    <vscode-text-field id="filter-field" placeholder="Search Configs" style="width:100%;display:none;position:sticky"></vscode-text-field>
+                    <vscode-text-field id="filter-field" placeholder="Search Configs..." style="width:100%;display:none;position:sticky"></vscode-text-field>
                 </div>
                 <div class="config-table-section">
                     <table></table>
@@ -19,7 +19,9 @@ export function getMainViewHtml(){
             </div>
         </div>
         <div id="content-frame">
-            <vscode-button id="show-code" style="float: right;margin: 2px 89px;">Show code</vscode-button>
+            <div class="show-button">
+                <vscode-button id="show-code" style="float: right;margin: 5px 25px; width:15%;border-radius:5px;border:1px solid #ffff">Show code</vscode-button>
+            </div>
             <div id="context-menu">
                 <div class="item">Delete</div>
             </div>
